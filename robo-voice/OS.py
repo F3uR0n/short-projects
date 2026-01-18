027118352de3cb1380755009e6cfb31501c325f3
Hustle <farhansadiknihal@gmail.com>
2026-01-19 00:54:24 +0600

    Initial Short Projects Final Update

--- robo-voice/OS.py
+++ robo-voice/OS.py
@@ -1,9 +1,13 @@
 import os
 
 if __name__ == '__main__':
-    print("Wlecome to Robo Voice")
+    print("Welcome to Robo Voice")
+
     while True:
         z = input("Enter what you want me to speak: ")
         if z == "end":
+            print("Goodbye 👋")
+            break
+
         command = f"say {z}"
         os.system(command)
